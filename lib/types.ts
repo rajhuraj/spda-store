52d1500804362b533316ac28a38404bc28a6c1ce
rajhuraj <raghu6092s@gmail.com>
2026-07-03 15:00:52 +0530

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -2,9 +2,10 @@ export type Product = {
   id: string;
   name: string;
   category: string;
-  price: string;
+  price: string | null;
   mrp: string | null;
   image_url: string | null;
+  images: string[];
   affiliate_link: string;
   source_url: string | null;
   created_at: string;
